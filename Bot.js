05f907602d2622642211d69bb74e7adfadf8a2bc
Nininanou16 <paul.peronredon@skypvp.fr>
2023-03-26 22:38:12 +0200

    add solar in client object

--- Bot.js
+++ Bot.js
@@ -5,6 +5,7 @@ const client = new Client({ intents: 131071 });
 client.commands = new Collection();
 client.buttons = new Collection();
 client.menus = new Collection();
+client.solar = {}
 
 module.exports.login = () => {
 	client.login(process.env.SECRET_TOKEN);
