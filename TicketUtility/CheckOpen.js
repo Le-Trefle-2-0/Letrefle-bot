a788e69329be0930390c659cb6926f0ddb54b049
Nininanou16 <paul.peronredon@skypvp.fr>
2023-03-26 23:31:02 +0200

    change api endpoint

--- TicketUtility/CheckOpen.js
+++ TicketUtility/CheckOpen.js
@@ -2,7 +2,7 @@ const {get} = require('axios')
 
 module.exports = (userID) => {
     return new Promise(async (resolve, reject) => {
-        await get(`${process.env.SOLAR_API}/listens/getInfo/${userID}`).then((res) => {
+        await get(`${process.env.SOLAR_API}/listens/findByUser/${userID}`).then((res) => {
             if (res.data == null || res.data == "" || res.data == undefined) reject(false);
             resolve(res.data);
         });
