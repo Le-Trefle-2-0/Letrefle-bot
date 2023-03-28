fb17bd33230ad555995e388b2a82582e48d077dc
Nininanou16 <paul.peronredon@skypvp.fr>
2023-03-28 19:34:01 +0200

    add to user message transmission

--- Solar/WebSocket.js
+++ Solar/WebSocket.js
@@ -1,14 +1,15 @@
 const {io} = require('socket.io-client');
+const {EmbedBuilder} = require('discord.js');
 
 module.exports.connect = async (Client) => {
-    const socket = await io(`ws://${process.env.SOLAR_WS}:3000`);
+    const socket = await io(`ws://${process.env.SOLAR_WS}:${process.env.SOLAR_WS_PORT}`);
 
     Client.solar.ws = socket;
 
     // socket.emit('send_message', );
 
     socket.on('hello', (arg) => {
-        Client.log.warning('WebSocket connected !');
+        Client.log.info('WebSocket connected !');
     });
 
     socket.emit('bot_connect', {});
@@ -17,7 +18,21 @@ module.exports.connect = async (Client) => {
         console.log(arg)
     });
 
+    socket.on('bot_message', async (message) => {
+        Client.log.info(`[MESSAGE] ${message.userID}: ${message.content}`)
+        let user = await Client.users.fetch(message.userID);
+        if (user) user.send({
+            embeds: [ 
+                new EmbedBuilder()
+                    .setAuthor({ name: 'Bénévole Écoutant' })
+                    .setDescription(message.content)
+                    .setTimestamp()
+                    .setColor('#8CBF86')
+            ]
+        });
+    });
+
     socket.onAny((event, ...args) => {
-        Client.log.info(event, args);
+        // Client.log.info(event, args);
     });
 }
