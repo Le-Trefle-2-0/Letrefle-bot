314f1648cb59fbf7205591c489010f92d21317de
Nininanou16 <paul.peronredon@skypvp.fr>
2023-10-24 22:40:26 +0200

    update to new login system

--- Solar/Login.js
+++ Solar/Login.js
@@ -2,7 +2,7 @@ const {post} = require('axios')
 
 module.exports = () => {
     post(`${process.env.SOLAR_API}/auth/login`, {
-        name: process.env.SOLAR_USERNAME,
+        email: process.env.SOLAR_USERNAME,
         password: process.env.SOLAR_PASSWORD
     }).then((response) => {
         console.log(response.data)
