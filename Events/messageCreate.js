35d14d90b51c27f4bcb6ba529c1c7cd206802d0c
Nininanou16 <paul@peronredon.eu>
2025-06-29 14:01:41 +0200

    handle message recieving

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -1,3 +1,40 @@
-module.exports = async (Client, message) => {
+const { ChannelType } = require('discord.js');
 
-}
+module.exports = async (Client, message) => {
+    if (message.author.bot) return;
+
+    if (message.channel.type === ChannelType.DM) {
+        const Ticket = Client.Solar.tickets.cache.find(ticket => ticket.discordUserID === message.author.id);
+        if (Ticket) {
+            Client.Solar.socket.emit('sendMessage', {
+                author: {
+                    id: Client.Solar.user.id,
+                    name: Client.Solar.user.displayUsername || Client.Solar.user.name,
+                    image: '/logo.svg'
+                },
+                content: message.content,
+                timestamp: Date.now(),
+                channel: {
+                    id: Ticket.channelId
+                }
+            });
+
+            fetch(`${Client.settings.solar.url}/api/messages/${Ticket.channelId}`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    content: message.content,
+                    timestamp: Date.now(),
+                    channel: {
+                        id: Ticket.channelId
+                    },
+                    author: {
+                        id: Client.Solar.user.id,
+                        name: Client.Solar.user.displayUsername || Client.Solar.user.name,
+                        image: '/logo.svg'
+                    },
+                    token: process.env.SOLAR_TOKEN
+                }),
+            })
+        }
+    }
+};
